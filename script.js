4928560ff9400bd7c3dc2e72567978fb0f77df13
robin-england <robin.england94@googlemail.com>
2024-06-17 16:12:16 -0400

    Configure linear scale for bar widths.

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ function main(){
     let margin = 1
     let barHeight = 20
     
-    
+    let scale = d3.scaleLinear()
+                    .domain([d3.min(data),d3.max(data)])    // Includes entirety of data
+                    .range([50,500])                        // min of 50, max of 500
+
     d3.select('body')
-        .append('svg')
+        .append('svg')      // Creates svg
         .attr('width', width)
-        .attr('height', barHeight*data.length + (data.length-1)*margin)
+        .attr('height', barHeight*data.length + (data.length-1)*margin) // 20 pixels per data point and 1 pixel margin between each data point
 
 }
