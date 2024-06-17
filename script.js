6db26bd2b2b5c90b91bad56aa67092032166b008
robin-england <robin.england94@googlemail.com>
2024-06-17 17:52:09 -0400

    Implement transitions and hover effects for bars.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// U63166005
+// Coding Challenge #11
+
 function main(){
 
     let data = [100, 420, 230, 850, 560, 925]
@@ -14,32 +17,49 @@ function main(){
         .attr('width', width)
         .attr('height', barHeight*data.length - margin) // 21 pixels per data point, minus 1 margin as only 5 margins are needed
 
+    let group = d3.select('svg')
+                    .selectAll('g')
+                    .data(data)
+                    .enter()
+                    .append('g')    // creates groups
+                    .attr('transform',function(d,i){
+                        return 'translate(0,'+ i*barHeight + ')'    // sets location for group elements
+                    })
 
-let group = d3.select('svg')
-                .selectAll('g')
-                .data(data)
-                .enter()
-                .append('g')    // Creates groups
-                .attr('transform',function(d,i){
-                    return 'translate(0,'+ i*barHeight + ')'    // sets location for group elements
-                })
+    group.append('rect')
+                    .attr('fill', 'darkgreen')
+                    .attr('height', barHeight - margin) // sets height of rectangles
+                    .transition()
+                    .ease(d3.easeLinear)                // loads bar in from left
+                    .duration(1000)
+                    .attr('width',function(d){
+                        return scale(d)                 // scales width of rectangles
+                    })
 
-group.append('rect')
-                .attr('fill', 'darkgreen')
-                .attr('width',function(d){
-                    return scale(d)                 // scales width of rectangles
-                })
-                .attr('height', barHeight - margin) // sets height of rectangles
+    group.append('text')
+                    .attr('stroke', 'black')        // sets color of text
+                    .attr('text-anchor', 'end')     // moves text to the end of bars
+                    .attr('y',barHeight/2)
+                    .attr('dy','.30em')
+                    .text(function(d){
+                        return d                    // sets value of text
+                    })
+                    .transition()
+                    .ease(d3.easeLinear)            // Loads bar in from left
+                    .duration(1000)
+                    .delay(1250)                    // loads in text after bars have reached full width
+                    .attr('x',function(d){          // sets location of text
+                        return(scale(d))
+                    });
 
-group.append('text')
-                .attr('stroke', 'white')        // sets color of text
-                .attr('text-anchor', 'end')     // moves text to the end of bars
-                .attr('x',function(d){          // sets location of text
-                    return(scale(d))
-                })
-                .attr('y',barHeight/2)
-                .attr('dy','.30em')
-                .text(function(d){
-                    return d                    // sets value of text
-                })
+    group.selectAll("rect")
+    .on("mouseover", function(){                    // sets event listener
+        d3.select(this)
+            .style("fill","lightgreen")             // changes color
+        })
+    .on("mouseout", 
+        function(){
+            d3.select(this)
+            .style("fill","darkgreen")
+        })
 }
